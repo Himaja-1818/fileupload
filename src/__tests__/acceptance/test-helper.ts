18ef878a414152bec61ced73bad6d6f5ba690252
himaja <himajagdr.1818@gmail.com>
2020-04-02 19:20:54 +0530

    upload

---
+++ src/__tests__/acceptance/test-helper.ts
@@ -0,0 +1,45 @@
+// Copyright IBM Corp. 2020. All Rights Reserved.
+// Node module: @loopback/example-file-transfer
+// This file is licensed under the MIT License.
+// License text available at https://opensource.org/licenses/MIT
+
+import {
+  Client,
+  createRestAppClient,
+  givenHttpServerConfig,
+  TestSandbox,
+} from '@loopback/testlab';
+import path from 'path';
+import {FileUploadApplication} from '../..';
+
+export async function setupApplication(): Promise<AppWithClient> {
+  const restConfig = givenHttpServerConfig({
+    // Customize the server configuration here.
+    // Empty values (undefined, '') will be ignored by the helper.
+    //
+    // host: process.env.HOST,
+    // port: +process.env.PORT,
+  });
+
+  const app = new FileUploadApplication({
+    rest: restConfig,
+  });
+
+  await app.boot();
+  await app.start();
+
+  const client = createRestAppClient(app);
+
+  return {app, client};
+}
+
+export interface AppWithClient {
+  app: FileUploadApplication;
+  client: Client;
+}
+
+const SANDBOX = path.resolve(__dirname, '../../../.sandbox');
+export function getSandbox() {
+  const sandbox = new TestSandbox(SANDBOX);
+  return sandbox;
+}
